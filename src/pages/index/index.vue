d19d9d82a5dd84e0d8d2af32a26eff0ee7059ac5
shiyangzhang <shiyangzhang@whale.im>
2023-02-23 17:18:47 +0800

    HEAD:

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <button on:click={{ddd}}>click</button>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.less'
+
+export default {
+  setup () {
+    const msg = ref('Hello world 1')
+    return {
+      msg
+    }
+  }
+}
+</script>
