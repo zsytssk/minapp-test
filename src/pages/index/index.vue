72d7452189cf5b9fd9bb4d86179d953304e024b6
shiyangzhang <shiyangzhang@whale.im>
2023-02-25 13:12:27 +0800

    main:

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -8,9 +8,11 @@
 <script>
 import { ref } from 'vue'
 import './index.less'
+import Taro from '@tarojs/taro'
 
 export default {
   setup () {
+    global.Taro = Taro
     const msg = ref('Hello world 1')
     return {
       msg
